3ef20fae4805c2e6886bd98474de7a6c0bf8eb73
Mohammad <moshafie@eng.ucsd.edu>
2022-04-25 09:19:16 -0700

    quick fix

--- experiments/run.sh
+++ experiments/run.sh
@@ -5,9 +5,8 @@ chmod 400 ~/.ssh/id_rsa
 apt update
 apt install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
-apt install -y python3.7
+apt install -y python3.7 python3.7-distutils
 source ./venv/bin/activate
-apt-get install python3.7-distutils
 pip install --upgrade pip
 pip3 install imageio tensorflow==1.13.1 tensorflow-gpu==1.13.1 scikit-image==0.15.0 tqdm
 sudo apt-get -y install exiftool
