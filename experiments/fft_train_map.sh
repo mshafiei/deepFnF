0f24b0b1e46723f19b79938e540b81ee73e55ca6
Mohammad <moshafie@eng.ucsd.edu>
2022-04-25 08:54:49 -0700

    quick fix

--- experiments/fft_train_map.sh
+++ experiments/fft_train_map.sh
@@ -3,8 +3,8 @@ exp_params="\
 --TLIST ./data/train_1600.txt \
 --VPATH ./data/valset/"
 
-name=msh-deepfnf-retrain
+name=msh-deepfnf-retrain4
 scriptFn="train.py $exp_params"
 
-./experiments/run_local.sh "$scriptFn"
-# ./experiments/run_server.sh "$scriptFn" "$name"
+# ./experiments/run_local.sh "$scriptFn"
+./experiments/run_server.sh "$scriptFn" "$name"
