cfb6f65731dc59abcdd3b278941dc1b03d888465
Mohammad <moshafie@eng.ucsd.edu>
2022-04-25 10:12:01 -0700

    fix

--- experiments/run.sh
+++ experiments/run.sh
@@ -10,10 +10,10 @@ apt install -y python3.7 python3.7-distutils python3.7-venv exiftool
 # pip install --upgrade pip
 python3.7 -m pip install --upgrade pip
 python3.7 -m pip install imageio tensorflow==1.13.1 tensorflow-gpu==1.13.1 scikit-image==0.16.2 tqdm PyExifTool piq lpips plotly==5.6.0 pandas kaleido
-python3 -c """import imageio
+python3.7 -c """import imageio
 imageio.plugins.freeimage.download()
 """
 export PYTHONPATH=`pwd`:/mshvol2/users/mohammad/cvgutils/
 echo command:
-echo $@
+echo python3.7 $@
 python3.7 $@
