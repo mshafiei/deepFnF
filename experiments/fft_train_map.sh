e4be74ac6748f7a8d4decb2a307bde48cdf9da60
Mohammad <moshafie@eng.ucsd.edu>
2022-04-26 12:56:21 -0700

    fix

--- experiments/fft_train_map.sh
+++ experiments/fft_train_map.sh
@@ -3,7 +3,7 @@ exp_params="\
 --TLIST ./data/train_1600.txt \
 --VPATH ./data/valset/"
 
-name=msh-deepfnf-retrain2
+name=msh-deepfnf-retrain0
 scriptFn="train.py $exp_params"
 
 # ./experiments/run_local.sh "$scriptFn"
--- experiments/run.sh
+++ experiments/run.sh
@@ -2,19 +2,20 @@
 cd /mshvol2/users/mohammad/optimization/deepfnf_fork
 cp /root/ssh_mount/id_rsa* /root/.ssh/
 chmod 400 ~/.ssh/id_rsa
-apt update
-apt install -y software-properties-common
-add-apt-repository -y ppa:deadsnakes/ppa
-apt install -y python3.7 python3.7-distutils python3.7-venv exiftool
-# source ./venv/bin/activate
-# pip install --upgrade pip
-python3.7 -m pip install --upgrade pip
-python3.7 -m pip install setuptools 
-python3.7 -m pip install imageio tensorflow-gpu==1.15.0 scikit-image==0.16.2 tqdm PyExifTool piq lpips plotly==5.6.0 pandas kaleido
+# apt update
+# apt install -y software-properties-common
+# add-apt-repository -y ppa:deadsnakes/ppa
+# apt install -y python3.7 python3.7-distutils python3.7-venv exiftool nvidia-cuda-toolkit
+# # source ./venv/bin/activate
+# # pip install --upgrade pip
+# python3.7 -m pip install --upgrade pip
+# python3.7 -m pip install setuptools 
+# python3.7 -m pip install imageio tensorflow-gpu==1.15.0 scikit-image==0.16.2 tqdm PyExifTool piq lpips plotly==5.6.0 pandas kaleido
 python3.7 -c """import imageio
 imageio.plugins.freeimage.download()
 """
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:/usr/local/cuda-10.0/:$LD_LIBRARY_PATH
 export PYTHONPATH=`pwd`:/mshvol2/users/mohammad/cvgutils/
 echo command:
-echo python3.7 $@
-python3.7 $@
+echo python3 $@
+python3 $@
