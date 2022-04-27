51640eb0b9357e6b4de267e7d3a1e6e9dfe81b97
Mohammad <moshafie@eng.ucsd.edu>
2022-04-27 15:44:15 -0700

    added install some libraries

--- experiments/fft_deepfnf_train.sh
+++ experiments/fft_deepfnf_train.sh
@@ -3,9 +3,9 @@ exp_params="\
 --TLIST ./data/train_1600.txt \
 --VPATH ./data/valset/ \
 --model deepfnf+fft \
---weight_dir fft_log"
+--weight_dir fft_log_init1"
 
-name=msh-deepfnf-fft-train0
+name=msh-deepfnf-fft-train-init1
 scriptFn="train.py $exp_params"
 
 # ./experiments/run_local.sh "$scriptFn"
--- experiments/run.sh
+++ experiments/run.sh
@@ -11,7 +11,8 @@ chmod 400 ~/.ssh/id_rsa
 # python3.7 -m pip install --upgrade pip
 # python3.7 -m pip install setuptools 
 # python3.7 -m pip install imageio tensorflow-gpu==1.15.0 scikit-image==0.16.2 tqdm PyExifTool piq lpips plotly==5.6.0 pandas kaleido
-python3.7 -c """import imageio
+pip3 install wandb natsort plotly pandas kaleido opencv-python
+python3 -c """import imageio
 imageio.plugins.freeimage.download()
 """
 export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:/usr/local/cuda-10.0/:$LD_LIBRARY_PATH
