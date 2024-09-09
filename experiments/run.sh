7f148e0324710d77585ddb12d1fce9ed3944996e
Mohammad <Mohammad>
2024-09-09 14:04:37 -0700

    updated run.sh

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,5 +1,31 @@
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
+#!/bin/bash
+. "/miniconda3/etc/profile.d/conda.sh"
+conda activate deepfnf
+sh_params=$@
+echo executing run.sh with arguments: $sh_params
+
+while [ $# -ge 1 ]; do
+        case "$1" in
+                --)
+                    # No more options left.
+                    shift
+                    break
+                   ;;
+                --use_gpu)
+                        use_gpu="$2"
+                        shift
+                        ;;
+        esac
+        shift
+done
+
+echo use_gpu in terminal $use_gpu
+
+if [[ $use_gpu == "False" ]]; then
+echo disabling gpu
+export CUDA_VISIBLE_DEVICES=""
+fi
+
 cd /mshvol2/users/mohammad/optimization/deepfnf_fork
 pip3 install imageio
 python3 -c """import imageio
