4cc0391344635b338f08d6ec4bef7360ab1c9203
Mohammad <Mohammad>
2025-02-16 19:49:00 -0800

    latest

--- tiny_unet_adjustable_fnf.py
+++ tiny_unet_adjustable_fnf.py
@@ -2,8 +2,8 @@ from tiny_unet_adjustable import Net as tiny_unet
 from easydict import EasyDict as edict
 import tensorflow as tf
 class Net(tiny_unet):
-    def __init__(self, downsample_ct, unet_output_size=3, channels_count_factor=1):
-        super().__init__(downsample_ct=downsample_ct, unet_output_size=unet_output_size, channels_count_factor=channels_count_factor)
+    def __init__(self, downsample_ct, unet_output_size=3, channels_count_factor=1,**kwargs):
+        super().__init__(downsample_ct=downsample_ct, unet_output_size=unet_output_size, channels_count_factor=channels_count_factor,**kwargs)
 
     @tf.function
     def forward(self, inp):
