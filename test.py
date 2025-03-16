f7bfd53b31e192c942b5e39825093e8d722a5b0b
Mohammad <Mohammad>
2025-03-15 20:21:35 -0700

    clip test image

--- test.py
+++ test.py
@@ -279,8 +279,9 @@ def test_single_image(data, logger, model, errval, metrics, metrics_list, errors
         images, lbls = model.visualize(model_inputs)
         
         # Save visualizations through logger
-        logger.addImage(images, lbls,os.path.basename(filename) + ('_%s' % (levelKey)),cols=6, annotation={'output':annotation}, mode='test', image_filename=filename, font_size_scale=2,vertical_spacing_scale=2, subtext=subtext)
-        logger.addIndividualImages({'output':images['output']}, {'output':lbls['output']})
+        name=os.path.basename(filename) + ('_%s' % (levelKey))
+        logger.addImage(images, lbls, name, cols=6, annotation={'output':annotation}, mode='test', image_filename=filename, font_size_scale=2,vertical_spacing_scale=2, subtext=subtext)
+        logger.addIndividualImages({name:tf.clip_by_value(images['output'],0,1)}, {name:lbls['output']})
         
         # Restore previous eager execution state
         if(not eagerly_state):
